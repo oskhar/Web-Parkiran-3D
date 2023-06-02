c5bb3c250f59a532a1410e64113f221373d817f0
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-06-02 22:58:35 +0700

    Membuat dan menambahkan objek 3D

---
+++ src/my_js/Model.js
@@ -0,0 +1,37 @@
+// ModelLamp
+var lampData = [
+    // {
+    //     "path": "./lib/asset_3d/lightpost.glb",
+    //     "color": 0x0066ff,
+    //     "position": [3, -1, -40],
+    //     "rotation": [0, 0, 0],
+    //     "scale": [2.5, 2.5, 2.5],
+    //     "light": [3, 2, -39, 10, 1]
+    // }
+];
+
+// ModelObject
+var objectData = [
+    {
+        "path": "./assets/3D/GedungParkirC.glb",
+        "position": [0, -1, 0],
+        "rotation": [0, 0, 0],
+        "scale": [0, 0, 0]
+    }
+];
+
+// ModelAction
+var actionData = [
+    // {
+    //     "path": "./lib/img/areaAction1.png",
+    //     "position": [13, 0, -20],
+    //     "scale": [1, 1, 1]
+    // }
+];
+
+var areaData = [
+    [13, -20],
+    [1, -3]
+];
+
+export {lampData, objectData, actionData, areaData};
